54c215454f4a7a7f0c179317da50acec47f8b0e6
chetu0209 <chethansp0209@gmail.com>
2025-04-03 15:16:45 +0530

    if&else_condition

---
+++ if&else_condition.py
@@ -0,0 +1,10 @@
+signal=input("what is the color of signal:")
+
+if signal=="red":
+    print("Stop")
+elif signal=="yellow":
+    print("Get Ready")
+elif signal=="green":
+    print("Go")
+else:
+    print("Invalid Signal")
